2db285b2c4928dec8df755b62a955e6e83955e0b
Jean <jeanychoi@gmail.com>
2016-02-19 12:32:57 -0800

    added final project to portfolio

--- lab7/starter_code_lesson_07/jquery_code_along/js/main.js
+++ lab7/starter_code_lesson_07/jquery_code_along/js/main.js
@@ -8,21 +8,14 @@ $('#box2').removeClass('highlight');
 // 2. Select the anchor within #box3. Change the HTML to 'jQuery!'
 $('#box3').find('a').html('jQuery!');
 
-// 3. Add the highlight class to the h3 when #box1 is clicked (use the .addClass() method)
-$('#box1').on('click', function(){
- $('h3').addClass('highlight');
-});
-
 
 /**
 * Part 1 - Now you try:
 */
 
 // 1. Add the .highlight class to #box4 (use the .addClass() method)
-$('#box4').addClass('highlight');
 
 // 2. Make the text color of the strong tag within #box2 green (use the .css() method)
-$('#box2').find('strong').css('color', 'green');
 
 
 /**-----------------------------------------------------------------------------------------------*/
@@ -53,7 +46,3 @@ $('#box3 a').on('click', function () {
 // 1. When the h2 is clicked on
 	// a) Slide up the h2
 	// b) Add the snazzy class to the h1
-$('h2').on('click', function () {
-	$('h2').slideUp();
-	$('h1').addClass('snazzy');
-});
